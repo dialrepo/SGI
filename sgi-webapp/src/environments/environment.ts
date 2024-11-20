73781a7732bea30c0ab484516cd837d312ffa8ed
dialrepo <laura.matarrubia@unirioja.es>
2024-11-20 14:45:21 +0100

    Actualizacion version nov 2024

--- sgi-webapp/src/environments/environment.ts
+++ sgi-webapp/src/environments/environment.ts
@@ -35,7 +35,7 @@ export const environment = {
     mode: SgiAuthMode.Keycloak,
     ssoRealm: 'sgi',
     ssoClientId: 'front',
-    ssoUrl: 'http://demo.hercules-sgi.local/auth',
+    ssoUrl: 'http://sgi-auth:8080/auth',
     // InMemory  auth config
     inMemoryConfig: {
       userRefId: '',
